a5fe20eb9628491c849c08ef3de28d17c60b8a4c
jamescoder92 <kusimbajames92@gmail.com>
2026-04-14 14:44:32 +0300

    burger

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your solution in this file!
+var burgers = ["Cheeseburger", "Hamburger"]
+var featuredDrink = "Strawberry Milkshake";
+
+function addBurger() {
+  var newBurger = "Flatburger";
+  burgers.push(newBurger);
+}
+
+if (true) {
+    let anotherNewBurger = "Maple Bacon Burger";
+    burgers.push(anotherNewBurger);
+}
+
+function changeFeaturedDrink() {
+  featuredDrink = "The Javashake";
+}
